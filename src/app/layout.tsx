278018cc5f9a2e4037530edf94be819ecbf51fbd
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2025-01-08 19:29:44 -0300

    add Toaster component to Providers for improved notification handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,7 +29,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
-        <Toaster />
       </body>
     </html>
   );
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Toaster } from '@/components/ui/toaster';
 import { useTaskStore } from '@/lib/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -23,12 +24,14 @@ export function Providers({ children }: { children: React.ReactNode }) {
   const { theme } = useTaskStore();
 
   useEffect(() => {
-    document.documentElement.className = theme;
+    document.documentElement.classList.remove('light', 'dark');
+    document.documentElement.classList.add(theme);
   }, [theme]);
 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <Toaster />
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
